ccce85571d05b52ba16ce5fd6f91641459892496
JerseyPl <Delr1602@gmail.com>
2023-08-03 20:28:02 +0300

    fix

--- frontend/src/features/Question/api.ts
+++
@@ -1,7 +0,0 @@
-export const QuestionFetch = async (idCard: number): Promise<Message> => {
-  const res = await fetch(`api/cards/${idCard}`, {
-    method: "delete",
-  });
-  const data = await res.json();
-  return data;
-};
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App/App';
-
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App/App';
 import store from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-
-<BrowserRouter>
-  <Provider store={store}>
-   <App />
-  </Provider>
-</BrowserRouter>
-
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
